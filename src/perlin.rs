8ecd0a301d156a4d7e8a231dbe802151a3b45c0d
Matt Stavola <mstavola9@gmail.com>
2020-12-13 00:16:57 -0800

    Trilinear smoothing

--- src/perlin.rs
+++ src/perlin.rs
@@ -2,6 +2,8 @@ use crate::{util::RandomDouble, vec3::Vec3};
 use itertools::Itertools;
 use rand::{seq::SliceRandom, Rng};
 
+type TrilinearSample = [[[f64; 2]; 2]; 2];
+
 const POINT_COUNT: usize = 256;
 
 #[derive(Clone, Debug)]
@@ -29,17 +31,34 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Vec3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        let x = self.perm_x[i as usize];
-        let y = self.perm_y[j as usize];
-        let z = self.perm_z[k as usize];
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
 
-        let index = (x ^ y ^ z) as usize;
+        let mut sample: TrilinearSample = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let i = (i + di as i32) & 255;
+                    let j = (j + dj as i32) & 255;
+                    let k = (k + dk as i32) & 255;
 
-        self.random_floats[index]
+                    let x = self.perm_x[i as usize];
+                    let y = self.perm_y[j as usize];
+                    let z = self.perm_z[k as usize];
+
+                    let index = (x ^ y ^ z) as usize;
+
+                    sample[di][dj][dk] = self.random_floats[index];
+                }
+            }
+        }
+
+        trilinear_interp(sample, u, v, w)
     }
 }
 
@@ -53,4 +72,23 @@ fn generate_perm<T: Rng>(rng: &mut T) -> [i32; POINT_COUNT] {
     }
 
     p
+}
+
+fn trilinear_interp(sample: TrilinearSample, u: f64, v: f64, w: f64) -> f64 {
+    let mut accumulator = 0.0;
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                let color = sample[i][j][k];
+
+                let i = i as f64 * u + (1.0 - i as f64) * (1.0 - u);
+                let j = j as f64 * v + (1.0 - j as f64) * (1.0 - v);
+                let k = k as f64 * w + (1.0 - k as f64) * (1.0 - w);
+
+                accumulator += color * i * j * k;
+            }
+        }
+    }
+
+    accumulator
 }
